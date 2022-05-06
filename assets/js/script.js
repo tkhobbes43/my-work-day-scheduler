bb3846d9789081149a686154deb4fb83467f5f69
Tommy Lee <t.k.hobbes43@gmail.com>
2022-05-05 22:47:51 -0500

    psuedo coding my javascript so that I know exactly what it will execute for the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,23 @@
+// adding current date to jumbotron using Moment.js
 var date = moment().format('dddd, MMMM Do');
 $("#currentDay").text(date);
+
+$(document).ready(function() {
+    console.log("Ready!")
+ 
+// time blocks need to be color coded to indicate whether the saved task is in the past, present, or future
+
+// need to be able to enter an even/task in each time block
+
+
+// when you click the save button, it gets saved in local storage
+
+
+// when you refresh the page, the saved events persists
+
+
+
+
+
+
+});
